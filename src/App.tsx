59e906ddd8f44d144103c14737c5ad36076b0c9a
maed <maed012014@gmail.com>
2023-05-02 14:06:54 -0600

    Added bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <></>;
+  return (
+    <main className="min-h-screen bg-blue-950 overflow-hidden">
+      <div className="blue__gradient h-[250px] w-[250px] sm:h-[500px] sm:w-[500px] rounded-full absolute left-[50%] translate-x-[-50%] -top-[100px] sm:-top-[250px]" />
+    </main>
+  );
 }
 
 export default App;
